8785f6ca742c656c91276c78a23977087b2bc874
zktzktzkt <550119647@qq.com>
2017-06-07 19:35:42 +0800

    吃包子dialog背景透明

--- app/src/main/java/demo/zkttestdemo/effect/loadingdialog/CustomProgressDialog.java
+++ app/src/main/java/demo/zkttestdemo/effect/loadingdialog/CustomProgressDialog.java
@@ -3,6 +3,7 @@ package demo.zkttestdemo.effect.loadingdialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,6 +21,7 @@ public class CustomProgressDialog extends ProgressDialog {
     private int  resId;  //资源id
     private String tips;  //需要显示的文本信息
 
+
     public CustomProgressDialog(Context context, String tips, int resId) {
         super(context);
 
@@ -31,6 +33,8 @@ public class CustomProgressDialog extends ProgressDialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.progress_dialog);
+        //设置透明背景
+        getWindow().setBackgroundDrawable(new ColorDrawable());
 
         loading_iv = (ImageView) findViewById(R.id.loading_iv);
         loading_tv = (TextView) findViewById(R.id.loading_tv);
