87a94e5789fca4f51d61a937f3aefe10bf2eda24
zktzktzkt <550119647@qq.com>
2017-12-07 12:01:39 +0800

    stikyhead列表：竖直rv嵌水平rv

--- app/src/main/java/demo/zkttestdemo/effect/stikyhead/StikyHeadActivity.java
+++ app/src/main/java/demo/zkttestdemo/effect/stikyhead/StikyHeadActivity.java
@@ -2,6 +2,7 @@ package demo.zkttestdemo.effect.stikyhead;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -19,7 +20,7 @@ public class StikyHeadActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stiky_head);
 
-        List<String> list = new ArrayList<>();
+        final List<String> list = new ArrayList<>();
         list.add("123");
         list.add("123");
         list.add("123");
@@ -38,10 +39,20 @@ public class StikyHeadActivity extends AppCompatActivity {
         list.add("123");
         list.add("123");
 
-        RecyclerView recycler = (RecyclerView) findViewById(R.id.recyclerView);
+        RecyclerView recycler =  findViewById(R.id.recyclerView);
         recycler.setAdapter(new BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_recorder, list) {
             @Override
             protected void convert(BaseViewHolder helper, String item) {
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(StikyHeadActivity.this);
+                linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+                RecyclerView recyclerView = helper.getView(R.id.rv_horizontal);
+                recyclerView.setLayoutManager(linearLayoutManager);
+
+                recyclerView.setAdapter(new BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_image, list) {
+                    @Override
+                    protected void convert(BaseViewHolder helper, String item) {
+                    }
+                });
 
             }
         });
