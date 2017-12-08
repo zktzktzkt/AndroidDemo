f299894995fefad2f52526c8918a59e68edc89c9
zktzktzkt <550119647@qq.com>
2017-12-09 04:09:35 +0800

    stikyHead coodinator试验

---
+++ app/src/main/java/demo/zkttestdemo/effect/stikyhead/ScrollableCoordinatorLayout.java
@@ -0,0 +1,36 @@
+package demo.zkttestdemo.effect.stikyhead;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Frank on 2016/11/24.
+ */
+
+public class ScrollableCoordinatorLayout extends CoordinatorLayout {
+
+    private boolean allowForScroll = false;
+
+    public ScrollableCoordinatorLayout(Context context) {
+        super(context);
+    }
+
+    public ScrollableCoordinatorLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
+        return allowForScroll && super.onStartNestedScroll(child, target, nestedScrollAxes);
+    }
+
+    public boolean isallowForScroll() {
+        return allowForScroll;
+    }
+
+    public void setallowForScroll(boolean allowForScroll) {
+        this.allowForScroll = allowForScroll;
+    }
+}
