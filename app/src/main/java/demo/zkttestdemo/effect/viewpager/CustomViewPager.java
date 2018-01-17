acb823b8f119355c382521247458c49e55307cd7
zktzktzkt <550119647@qq.com>
2018-01-17 11:59:41 +0800

    自定义viewpager

---
+++ app/src/main/java/demo/zkttestdemo/effect/viewpager/CustomViewPager.java
@@ -0,0 +1,197 @@
+package demo.zkttestdemo.effect.viewpager;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Scroller;
+
+/**
+ * Created by zkt on 2018-1-17.
+ */
+
+public class CustomViewPager extends ViewGroup {
+
+    private Scroller scroller;
+    private VelocityTracker tracker;
+    private int lastInterceptX = 0;
+    private int lastInterceptY = 0;
+    private int lastX;
+    private int lastY;
+    private int currentIndex = 0; //当前子元素
+    private int childWidth = 0;
+
+
+    public CustomViewPager(Context context) {
+        super(context);
+        init();
+    }
+
+    public CustomViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomViewPager(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        scroller = new Scroller(getContext());
+        tracker = VelocityTracker.obtain();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        //测量所有子元素
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+        //处理wrap_content
+        if (getChildCount() == 0) {
+            setMeasuredDimension(0, 0);
+        }
+        //宽和高都是AT_MOST，则设置宽度所有子元素的宽度的和；高度设置为第一个元素的高度；
+        else if (widthMode == MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST) {
+            View childOne = getChildAt(0);
+            int childWidth = childOne.getMeasuredWidth();
+            int childHeight = childOne.getMeasuredHeight();
+            setMeasuredDimension(childWidth * getChildCount(), childHeight);
+
+        } else if (widthMode == MeasureSpec.AT_MOST) {
+            int childWidth = getChildAt(0).getMeasuredWidth();
+            setMeasuredDimension(childWidth * getChildCount(), heightSize);
+
+        } else if (heightMode == MeasureSpec.AT_MOST) {
+            int childHeight = getChildAt(0).getMeasuredHeight();
+            setMeasuredDimension(widthSize, childHeight);
+        }
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int childCount = getChildCount();
+        int left = 0;
+        View child;
+        for(int i = 0; i < childCount; i++){
+            child = getChildAt(i);
+            if(child.getVisibility() != GONE){
+                int width = child.getMeasuredWidth();
+                childWidth = width;
+                child.layout(left, 0, left + width, child.getMeasuredHeight());
+                left += width;
+            }
+        }
+    }
+
+    /**
+     * 事件分发分两种情况
+     * 1. 子view处理了down事件，所以viewGroup的onTouchEvent不会被触发；
+     * 2. 子view没有处理down事件，所以事件会被回溯到viewGroup的onTouchEvent，
+     * 2.1  如果onTouchEvent为true，则接下来的事件都由自己处理
+     * 2.2  否则事件接着向上回溯
+     * <p>
+     * 子View消费了事件，所以ViewGroup的onInterceptTouchEvent的‘MOVE’会被调用
+     * 子view没有消费事件，所以ViewGroup的onTouchEvent会被调用
+     */
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        boolean intercept = false;
+        int x = (int) ev.getX();
+        int y = (int) ev.getY();
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                intercept = false;
+                //如果动画还没有执行完成，则打断
+                if (!scroller.isFinished()) {
+                    scroller.abortAnimation();
+                }
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = x - lastInterceptX;
+                int deltaY = y - lastInterceptY;
+                //如果是横向滑动，就拦截，交由自己处理
+                if (Math.abs(deltaX) - Math.abs(deltaY) > 0) {
+                    intercept = true;
+                } else {
+                    intercept = false;
+                }
+                break;
+
+            case MotionEvent.ACTION_UP:
+                intercept = false;
+                break;
+        }
+        //因为DOWN返回false，所以onTouchEvent中无法获取DOWN事件，这里负责设置lastX，lastY
+        lastX = x;
+        lastY = y;
+        lastInterceptX = x;
+        lastInterceptY = y;
+        return intercept;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        tracker.addMovement(event);
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (!scroller.isFinished()) {
+                    scroller.abortAnimation();
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = x - lastX;
+                scrollBy(-deltaX, 0);
+                break;
+            case MotionEvent.ACTION_UP:
+                //相对于当前View滑动的距离，正向左，负向右
+                int distance = getScrollX() - currentIndex * childWidth;
+                if (Math.abs(distance) > childWidth / 2) {
+                    if (distance > 0) {
+                        currentIndex++;
+                    } else {
+                        currentIndex--;
+                    }
+                } else {
+                    tracker.computeCurrentVelocity(1000);
+                    float xVelocity = tracker.getXVelocity();
+                    if (Math.abs(xVelocity) > 50) {
+                        if (xVelocity > 0) {
+                            currentIndex--;
+                        } else {
+                            currentIndex++;
+                        }
+                    }
+                }
+                currentIndex = currentIndex < 0 ? 0 : currentIndex > getChildCount() - 1 ? getChildCount() - 1 : currentIndex;
+                smoothScrollTo(currentIndex * childWidth, 0);
+                tracker.clear();
+                break;
+        }
+        return true;
+    }
+
+    private void smoothScrollTo(int destX, int destY) {
+        scroller.startScroll(getScrollX(), getScrollY(), destX - getScrollX(), destY - getScrollY(), 1000);
+        invalidate();
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (scroller.computeScrollOffset()) {
+            scrollTo(scroller.getCurrX(), scroller.getCurrY());
+            postInvalidate();
+        }
+    }
+}
