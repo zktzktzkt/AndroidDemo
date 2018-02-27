b38e827bdc42813ae562e22ecbc1bcaab0ac104e
zktzktzkt <550119647@qq.com>
2018-02-27 11:16:03 +0800

    温度控制旋钮

--- app/src/main/java/demo/zkttestdemo/effect/kugou/KuGouParallaxActivity.java
+++ app/src/main/java/demo/zkttestdemo/effect/kugou/KuGouParallaxActivity.java
@@ -12,4 +12,6 @@ public class KuGouParallaxActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ku_gou_parallax);
     }
+
+    
 }
