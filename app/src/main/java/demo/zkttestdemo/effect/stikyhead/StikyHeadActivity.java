b67e2bf97df9689c0c03f56c300687c334ea03fe
zktzktzkt <550119647@qq.com>
2021-06-04 22:14:11 +0800

    模拟手势滑动

--- app/src/main/java/demo/zkttestdemo/effect/stikyhead/StikyHeadActivity.java
+++ app/src/main/java/demo/zkttestdemo/effect/stikyhead/StikyHeadActivity.java
@@ -1,6 +1,10 @@
 package demo.zkttestdemo.effect.stikyhead;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.MotionEvent;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,6 +21,8 @@ import demo.zkttestdemo.R;
  * CoordinatorLayout实现悬浮头
  */
 public class StikyHeadActivity extends AppCompatActivity {
+
+    private RecyclerView recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +48,7 @@ public class StikyHeadActivity extends AppCompatActivity {
         list.add("123");
         list.add("123");
 
-        RecyclerView recycler = findViewById(R.id.recyclerView);
+        recycler = findViewById(R.id.recyclerView);
         recycler.setAdapter(new BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_recorder, list) {
             @Override
             protected void convert(BaseViewHolder helper, String item) {
@@ -59,5 +65,27 @@ public class StikyHeadActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    /**测试滚动*/
+    public void onClickScroll(View view) {
+        int        y        = findViewById(R.id.root).getHeight() - 1; //减一是因为刚好在边界值是没办法触发事件的
+        final long downTime = SystemClock.currentThreadTimeMillis();
+        final long distance = 300;
+
+        //按下
+        MotionEvent down = MotionEvent.obtain(downTime, SystemClock.currentThreadTimeMillis(),
+                                              MotionEvent.ACTION_DOWN, 0, y, 0);
+        dispatchTouchEvent(down);
+
+        //移动
+        MotionEvent move = MotionEvent.obtain(downTime, SystemClock.currentThreadTimeMillis() + 50,
+                                              MotionEvent.ACTION_MOVE, 0, y - distance, 0);
+        dispatchTouchEvent(move);
+
+        //抬起
+        MotionEvent up = MotionEvent.obtain(downTime, SystemClock.currentThreadTimeMillis() + 50,
+                                            MotionEvent.ACTION_UP, 0, y - distance, 0);
+        dispatchTouchEvent(up);
     }
 }
