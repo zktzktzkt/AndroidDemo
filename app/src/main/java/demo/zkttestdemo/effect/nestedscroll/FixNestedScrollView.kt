89415b3fc71deba956400b014c7394df1f6e0822
周楷桐 <550119647@qq.com>
2022-04-01 01:01:18 +0800

    修复child的fling不停，导致哪怕parent已经到顶了，而onNestedPreScroll还一直在回调

--- app/src/main/java/demo/zkttestdemo/effect/nestedscroll/FixNestedScrollView.kt
+++ app/src/main/java/demo/zkttestdemo/effect/nestedscroll/FixNestedScrollView.kt
@@ -5,7 +5,6 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import android.view.ViewGroup
 import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -17,8 +16,8 @@ class FixNestedScrollView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : NestedScrollView(context, attrs, defStyleAttr) {
 
+    private var mChildVelocityY: Float = 0.0f
     private var mChildRV: RecyclerView? = null
-    private var mTotalDistance: Double = 0.0
 
     //TODO 不必纠结这个数值，就是为了效果一点点调出来的，具体阈值的根据业务处理
     private var mDefaultValue: Int = 600
@@ -29,40 +28,65 @@ class FixNestedScrollView @JvmOverloads constructor(
     init {
 
         setOnScrollChangeListener(object : OnScrollChangeListener {
+
             override fun onScrollChange(v: NestedScrollView, scrollX: Int, scrollY: Int, oldScrollX: Int, oldScrollY: Int) {
 
+                //当child没有滑到顶的时候，控制外层不允许滑动
                 mChildRV?.let {
                     if (it.computeVerticalScrollOffset() > 0) {
                         scrollTo(0, mDefaultValue)
                         return
                     }
                 }
-
+                // 到达顶部的时候，允许下拉刷新
+                if (scrollY == 0) {
+                    // refreshLayout.setEnabled(true);
+                }
+                // 处理子View的fling
                 if (scrollY >= mDefaultValue && !v.canScrollVertically(1)) {
                     Log.e("FixNestedScrollView", "onScrollChange scrollY:${scrollY} 不能滑了")
-                    val remainDistance = mTotalDistance - scrollY
-                    val velocitY = mFlingHelper.getVelocityByDistance(remainDistance)
-                    childFling(velocitY)
+                    childFling()
                 }
             }
         })
     }
 
-    private fun childFling(velocitY: Int) {
-        mChildRV?.fling(0, velocitY)
+    private fun childFling() {
+        if (mVelocityY > 0) {
+            //速度转距离
+            val splineFlingDistance = mFlingHelper.getSplineFlingDistance(mVelocityY)
+            if (splineFlingDistance > mDefaultValue) {
+                // 距离转速度
+                val velocityByDistance = mFlingHelper.getVelocityByDistance(splineFlingDistance - mDefaultValue)
+                mChildRV?.fling(0, velocityByDistance)
+            }
+            mVelocityY = 0
+        }
     }
 
     fun setchildRV(recyclerView: RecyclerView) {
         mChildRV = recyclerView
     }
 
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        when (ev.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mChildRV?.stopScroll() //停止child的fling事件
+            }
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
+
     /**
      * 注意一定要用 NestedScrollingParent2 的方法，五个参数的
+     * 方法触发时机：child的MOVE事件和fling事件
      * @param dy 上滑正 下滑负
      */
     override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
         //super.onNestedPreScroll(target, dx, dy, consumed, type)
-        //Log.e("FixNestedScrollView", "view:${target.javaClass.simpleName} getTop:${target.top} dy:${dy} consumed[1]:${consumed[1]}  scrollY:${scrollY}")
+        Log.e("FixNestedScrollView", "view:${target.javaClass.simpleName} getTop:${target.top} dy:${dy} consumed[1]:${consumed[1]}  scrollY:${scrollY}")
         if (scrollY < mDefaultValue) {
             consumed[1] = dy
             scrollBy(0, dy)
@@ -73,15 +97,17 @@ class FixNestedScrollView @JvmOverloads constructor(
      * @return false接收fling，true不接收
      */
     override fun onNestedPreFling(target: View, velocityX: Float, velocityY: Float): Boolean {
+        mChildVelocityY = velocityY
         return false
     }
 
 
+    /**
+     * 自己的fling
+     */
     override fun fling(velocityY: Int) {
         super.fling(velocityY)
-        mVelocityY = velocityY
-        mTotalDistance = mFlingHelper.getSplineFlingDistance(velocityY)
-        Log.e("FixNestedScrollView", "fling滑动总距离->$mTotalDistance")
+        mVelocityY = if (velocityY <= 0) 0 else velocityY //滑动的速度
     }
 
 
